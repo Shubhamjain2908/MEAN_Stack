632930cb17f0d8646d3ff68b861134e30203938f
shubham jain <shubhamjain2908@gmail.com>
2018-10-08 00:59:09 +0530

    mongoose added, post api completed

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://shubhamjain2908:PmQW8uvx1xEUoq8R@oauth-v3ieo.mongodb.net/node-angular?retryWrites=true', { useNewUrlParser: true })
+  .then(() => {
+      console.log('Connected to database!');
+  })
+  .catch(()=>{
+    console.log('Connection failed!');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,8 +27,11 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
   res.status(201).json({
     message: 'Post added succeddfully'
   });
